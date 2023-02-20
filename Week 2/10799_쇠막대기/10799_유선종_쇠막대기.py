490c405ca70e33e118506da29d0ded2ba2cc7dbc
Ssunbell <yousun9591@naver.com>
2023-02-20 11:44:52 +0900

    선종:쇠막대기

---
+++ Week 2/10799_쇠막대기/10799_유선종_쇠막대기.py
@@ -0,0 +1,20 @@
+from typing import *
+
+def solution(array:List[str]):
+    s:List[str] = []
+    score:int = 0
+    for i, rod in enumerate(array):
+        if rod == '(':
+            s.append(rod)
+        else:
+            s.pop()
+            if array[i-1] == '(': # 레이저 지이이이잉
+                score += len(s)
+            else: # 쇠막대기의 끝부분 도달
+                score += 1
+    
+    return score
+
+array = list(input())
+print(solution(array))
+            
